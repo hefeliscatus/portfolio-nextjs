c074086e72fd6e3bf2054d04f6748bc126f3d303
Nithish Pravin <nithishdev509@gmail.com>
2021-03-14 12:57:35 +0530

    code formatted

--- pages/index.js
+++ pages/index.js
@@ -1,81 +1,126 @@
-import Head from 'next/head'
+import Head from "next/head";
 import { Helmet } from "react-helmet";
-import {AiFillGithub, AiFillYoutube} from 'react-icons/ai'
-import { FiInstagram } from 'react-icons/fi'
-import { FaDiscord } from 'react-icons/fa'
+import { AiFillGithub, AiFillYoutube } from "react-icons/ai";
+import { FiInstagram } from "react-icons/fi";
+import { FaDiscord } from "react-icons/fa";
 
 export default function Home() {
   return (
-      <>
-      <div style={{overflow: 'hidden'}}>
-          <div className="parallax"></div>
-          <h1 className={'mainTitle'}>Wassup, I'm Nithish Pravin</h1>
-          <h2 className={'subTitle'}>A <span className={'rainbow_text_animated'}>programmer</span> under 15 and a <span className={'rainbow_text_animated'}>weeb</span>, what else?</h2>
-          <h1 className={'typing'}>
-              I like to mess with&nbsp;
-                                      <span
-    className="txt-rotate"
-    data-period="2000"
-    data-rotate='[ "JavaScript", "NodeJs", "Java", "Flutter", "Python", "ReactJs" ]'/>
-          </h1>
-          <div className="social-buttons">
-
-          <a href="#" class="social-buttons__button social-button social-button--github" aria-label="Github">
+    <>
+      <div style={{ overflow: "hidden" }}>
+        <div className="parallax"></div>
+        <h1 className={"mainTitle"}>Wassup, I'm Nithish Pravin</h1>
+        <h2 className={"subTitle"}>
+          A <span className={"rainbow_text_animated"}>programmer</span> under 15
+          and a <span className={"rainbow_text_animated"}>weeb</span>, what
+          else?
+        </h2>
+        <h1 className={"typing"}>
+          I like to mess with&nbsp;
+          <span
+            className="txt-rotate"
+            data-period="2000"
+            data-rotate='[ "JavaScript", "NodeJs", "Java", "Flutter", "Python", "ReactJs" ]'
+          />
+        </h1>
+        <div className="social-buttons">
+          <a
+            href="#"
+            class="social-buttons__button social-button social-button--github"
+            aria-label="Github"
+          >
             <span class="social-button__inner">
-             <AiFillGithub/>
+              <AiFillGithub />
             </span>
           </a>
 
-          <a href="#" class="social-buttons__button social-button social-button--youtube" aria-label="Youtube">
+          <a
+            href="#"
+            class="social-buttons__button social-button social-button--youtube"
+            aria-label="Youtube"
+          >
             <span class="social-button__inner">
-             <AiFillYoutube/>
+              <AiFillYoutube />
             </span>
           </a>
 
-          <a href="#" class="social-buttons__button social-button social-button--instagram" aria-label="Instagram">
+          <a
+            href="#"
+            class="social-buttons__button social-button social-button--instagram"
+            aria-label="Instagram"
+          >
             <span class="social-button__inner">
-             <FiInstagram/>
+              <FiInstagram />
             </span>
           </a>
 
-          <a href="#" class="social-buttons__button social-button social-button--discord" aria-label="Discord">
+          <a
+            href="#"
+            class="social-buttons__button social-button social-button--discord"
+            aria-label="Discord"
+          >
             <span class="social-button__inner">
-             <FaDiscord/>
+              <FaDiscord />
             </span>
           </a>
-
+        </div>
+        <div className="window">
+          <div className="titlebar">
+            <div className="buttons">
+              <div className="close">
+                <a className="closebutton">
+                  <span className="spanterminal">
+                    <strong>x</strong>
+                  </span>
+                </a>
+              </div>
+              <div className="minimize">
+                <a className="minimizebutton">
+                  <span className="spanterminal">
+                    <strong>&ndash;</strong>
+                  </span>
+                </a>
+              </div>
+              <div className="zoom">
+                <a className="zoombutton">
+                  <span className="spanterminal">
+                    <strong>+</strong>
+                  </span>
+                </a>
+              </div>
+            </div>
+            <div
+              style={{
+                float: "center",
+                color: "#ffffff",
+                fontSize: "11px",
+                paddingTop: "3px",
+                lineHeight: "11px",
+                marginRight: "50px",
+              }}
+            >
+              Terminal - About-Me
+            </div>
           </div>
-          <div className="window">
-  <div className="titlebar">
-    <div className="buttons">
-      <div className="close">
-        <a className="closebutton"><span className="spanterminal"><strong>x</strong></span></a>
-      </div>
-      <div className="minimize">
-        <a className="minimizebutton"><span className="spanterminal"><strong>&ndash;</strong></span></a>
-      </div>
-      <div className="zoom">
-        <a className="zoombutton"><span className="spanterminal"><strong>+</strong></span></a>
-      </div>
-    </div>
-    <div style={{float: "center", color: "#ffffff", fontSize: "11px", paddingTop: '3px', lineHeight: "11px", marginRight: '50px'}}>
-    Terminal - About-Me</div>
-  </div>
-  <div className="content">
-    <h3 style={{marginTop: "0px"}}>D://Nithish//About:~ $ node</h3>
-    Welcome to Node.js v12.19.0.<br/>
-    Type ".help" for more information.<br/>
+          <div className="content">
+            <h3 style={{ marginTop: "0px" }}>D://Nithish//About:~ $ node</h3>
+            Welcome to Node.js v12.19.0.
+            <br />
+            Type ".help" for more information.
+            <br />
+            {">"} var about = require('./about-me.txt');
+            <br />
+            <span style={{ color: "grey" }}>undefined</span>
+            <br />
+            {">"} console.log(about);
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing
+          </div>
+        </div>
 
-    {'>'} var about = require('./about-me.txt');<br/>
-    <span style={{color: 'grey'}}>undefined</span><br/>
-    {'>'} console.log(about);<br/>
-    <br/>
-    Lorem ipsum dolor sit amet, consectetur adipiscing
-  </div>
-</div>
-
-          <Helmet>
-              <script>{`
+        <Helmet>
+          <script>{`
               var TxtRotate = function(el, toRotate, period) {
               this.toRotate = toRotate;
               this.el = el;
@@ -133,8 +178,8 @@ export default function Home() {
               document.body.appendChild(css);
           };
                   `}</script>
-          </Helmet>
+        </Helmet>
       </div>
-      </>
-  )
+    </>
+  );
 }
